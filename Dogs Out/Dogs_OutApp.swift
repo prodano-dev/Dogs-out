5dbe840248096a610305dd78d134b6075f7782e9
Danny Pro <danny.pro17@gmail.com>
2021-11-25 10:30:30 +0100

    first commit

---
+++ Dogs Out/Dogs_OutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dogs_OutApp.swift
+//  Dogs Out
+//
+//  Created by Danny Prodanovic on 25/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Dogs_OutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
